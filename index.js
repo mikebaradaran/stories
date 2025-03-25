71a68fa3b7233efc367f639663966649ced12455
Glitch (glitch-hello-website) <none>
2025-03-25 10:31:18 +0000

    🛍🙉 Checkpoint ./index.js:34363899/759

--- index.js
+++ index.js
@@ -5,4 +5,15 @@ for (let line of lines) {
     let div = document.createElement("div");
     div.innerHTML = line;
     container.appendChild(div);
+
+    let btnRead = document.createElement("button");
+    btnRead.innerHTML = " 🔊";
+    div.appendChild(btnRead);
+    btnRead.addEventListener('click', () =>
+        speak(line));
+}
+function speak(text) {
+    text = text.split("<icon>")[0].trim();
+    const speech = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(speech);
 }
